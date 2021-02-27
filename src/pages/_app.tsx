3598ba643cf8f67e3e1cadd920487012cc1bb5c1
Edson Borge <edsonborge90@gmail.com>
2021-02-27 11:51:41 -0300

    Challenge Box finished

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,13 @@
 import '../styles/global.css';
 
-import { ChallengesContext } from '../contexts/ChallengesContext';
+import { ChallengesProvider } from '../contexts/ChallengesContext';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChallengesContext.Provider value={'teste'}>
+    <ChallengesProvider>
       <Component {...pageProps} />
-    </ChallengesContext.Provider>
+    </ChallengesProvider>
   )
 }
 
-export default MyApp
+export default MyApp 
